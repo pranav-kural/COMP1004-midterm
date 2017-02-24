f4524e3aa2d0f0830946761efb7c62bb3d6e444b
Pranav Kural <pranavkural042@gmail.com>
2017-02-24 15:43:06 -0500

    Initial commit

---
+++ COMP1004-W2017-MidTermAssgnment-200333253/COMP1004-W2017-MidTermAssgnment-200333253/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_W2017_MidTermAssgnment_200333253
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AbilityForm());
+        }
+    }
+}

c74f95b8adf38ff794ed61c4eb1e05d542335271
Pranav Kural <pranavkural042@gmail.com>
2017-02-24 20:48:43 -0500

    Updated the JobForm GUI

--- COMP1004-W2017-MidTermAssgnment-200333253/COMP1004-W2017-MidTermAssgnment-200333253/JobForm.Designer.cs
+++ COMP1004-W2017-MidTermAssgnment-200333253/COMP1004-W2017-MidTermAssgnment-200333253/JobForm.Designer.cs
@@ -29,33 +29,134 @@
         private void InitializeComponent()
         {
             this.NextButton = new System.Windows.Forms.Button();
+            this.JobGroupBox = new System.Windows.Forms.GroupBox();
+            this.SoldierRadioButton = new System.Windows.Forms.RadioButton();
+            this.RogueRadioButton = new System.Windows.Forms.RadioButton();
+            this.MagickerRadioButton = new System.Windows.Forms.RadioButton();
+            this.CultistRadioButton = new System.Windows.Forms.RadioButton();
+            this.HealthLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.JobGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // NextButton
             // 
-            this.NextButton.Location = new System.Drawing.Point(525, 465);
-            this.NextButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NextButton.Location = new System.Drawing.Point(332, 243);
+            this.NextButton.Margin = new System.Windows.Forms.Padding(4);
             this.NextButton.Name = "NextButton";
             this.NextButton.Size = new System.Drawing.Size(100, 28);
             this.NextButton.TabIndex = 3;
             this.NextButton.Text = "Next";
             this.NextButton.UseVisualStyleBackColor = true;
             // 
+            // JobGroupBox
+            // 
+            this.JobGroupBox.Controls.Add(this.CultistRadioButton);
+            this.JobGroupBox.Controls.Add(this.MagickerRadioButton);
+            this.JobGroupBox.Controls.Add(this.RogueRadioButton);
+            this.JobGroupBox.Controls.Add(this.SoldierRadioButton);
+            this.JobGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JobGroupBox.Location = new System.Drawing.Point(32, 39);
+            this.JobGroupBox.Name = "JobGroupBox";
+            this.JobGroupBox.Size = new System.Drawing.Size(188, 232);
+            this.JobGroupBox.TabIndex = 4;
+            this.JobGroupBox.TabStop = false;
+            this.JobGroupBox.Text = "Select Player Job";
+            // 
+            // SoldierRadioButton
+            // 
+            this.SoldierRadioButton.AutoSize = true;
+            this.SoldierRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SoldierRadioButton.Location = new System.Drawing.Point(39, 47);
+            this.SoldierRadioButton.Name = "SoldierRadioButton";
+            this.SoldierRadioButton.Size = new System.Drawing.Size(76, 24);
+            this.SoldierRadioButton.TabIndex = 0;
+            this.SoldierRadioButton.TabStop = true;
+            this.SoldierRadioButton.Text = "Soldier";
+            this.SoldierRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // RogueRadioButton
+            // 
+            this.RogueRadioButton.AutoSize = true;
+            this.RogueRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RogueRadioButton.Location = new System.Drawing.Point(39, 91);
+            this.RogueRadioButton.Name = "RogueRadioButton";
+            this.RogueRadioButton.Size = new System.Drawing.Size(75, 24);
+            this.RogueRadioButton.TabIndex = 1;
+            this.RogueRadioButton.TabStop = true;
+            this.RogueRadioButton.Text = "Rogue";
+            this.RogueRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // MagickerRadioButton
+            // 
+            this.MagickerRadioButton.AutoSize = true;
+            this.MagickerRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MagickerRadioButton.Location = new System.Drawing.Point(39, 133);
+            this.MagickerRadioButton.Name = "MagickerRadioButton";
+            this.MagickerRadioButton.Size = new System.Drawing.Size(91, 24);
+            this.MagickerRadioButton.TabIndex = 2;
+            this.MagickerRadioButton.TabStop = true;
+            this.MagickerRadioButton.Text = "Magicker";
+            this.MagickerRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CultistRadioButton
+            // 
+            this.CultistRadioButton.AutoSize = true;
+            this.CultistRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CultistRadioButton.Location = new System.Drawing.Point(39, 180);
+            this.CultistRadioButton.Name = "CultistRadioButton";
+            this.CultistRadioButton.Size = new System.Drawing.Size(71, 24);
+            this.CultistRadioButton.TabIndex = 3;
+            this.CultistRadioButton.TabStop = true;
+            this.CultistRadioButton.Text = "Cultist";
+            this.CultistRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // HealthLabel
+            // 
+            this.HealthLabel.AutoSize = true;
+            this.HealthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HealthLabel.Location = new System.Drawing.Point(252, 39);
+            this.HealthLabel.Name = "HealthLabel";
+            this.HealthLabel.Size = new System.Drawing.Size(76, 24);
+            this.HealthLabel.TabIndex = 5;
+            this.HealthLabel.Text = "Health:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(256, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 29);
+            this.textBox1.TabIndex = 6;
+            // 
             // JobForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(641, 508);
+            this.ClientSize = new System.Drawing.Size(458, 303);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.HealthLabel);
+            this.Controls.Add(this.JobGroupBox);
             this.Controls.Add(this.NextButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "JobForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Select the Character\'s Job";
+            this.JobGroupBox.ResumeLayout(false);
+            this.JobGroupBox.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         internal System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.GroupBox JobGroupBox;
+        private System.Windows.Forms.RadioButton CultistRadioButton;
+        private System.Windows.Forms.RadioButton MagickerRadioButton;
+        private System.Windows.Forms.RadioButton RogueRadioButton;
+        private System.Windows.Forms.RadioButton SoldierRadioButton;
+        private System.Windows.Forms.Label HealthLabel;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- COMP1004-W2017-MidTermAssgnment-200333253/COMP1004-W2017-MidTermAssgnment-200333253/JobForm.cs
+++ COMP1004-W2017-MidTermAssgnment-200333253/COMP1004-W2017-MidTermAssgnment-200333253/JobForm.cs
@@ -12,9 +12,31 @@ namespace COMP1004_W2017_MidTermAssgnment_200333253
 {
     public partial class JobForm : Form
     {
+
+        // store the player information
+        private Dictionary<string, string> _playerInfo;
+
         public JobForm()
         {
             InitializeComponent();
+            _playerInfo = new Dictionary<string, string>();
         }
+
+        // set the player info (receive it from the AbilitiesForm)
+        public void SetPlayerInfo(Dictionary<string, string> info)
+        {
+            // get the values and set to the _playerInfo of RaceForm
+            foreach (KeyValuePair<string, string> pair in info)
+            {
+                this._playerInfo.Add(pair.Key, pair.Value);
+            }
+
+            // add the property of job to the _playerInfo
+            this._playerInfo.Add("job", "");
+        }
+
+
+
+
     }
 }
